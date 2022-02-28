5f8d9a458ee6ac2f8a94f81ff7814b33d72d446b
godChoi96 <oinkoink71@naver.com>
2022-02-28 11:43:06 +0900

    error

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+    direction: "vertical",
+    sliderPerView: 1,
+    spaceBetween: 0,
+    mousewheel: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar',
+    }
+}) 
